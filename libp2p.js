27fc1e834d324f2a005d086a954ac63dfaf1b198
Shakir Mengrani <shakir.mengrani@gmail.com>
2021-04-18 13:23:29 +0500

    libp2p chat with multiple nodes

--- libp2p.js
+++ libp2p.js
@@ -1,6 +1,6 @@
+const repl = require("repl")
 const fs = require("fs");
 const peerId = require("peer-id");
-const { createInterface } = require("readline");
 const { pipe } = require("it-pipe")
 const Libp2p = require("libp2p");
 const TCP = require("libp2p-tcp");
@@ -8,22 +8,43 @@ const WS = require("libp2p-websockets")
 const mplex = require('libp2p-mplex')
 const { NOISE } = require("libp2p-noise");
 const MDNS = require("libp2p-mdns")
-
+let peerStreams = [];
 const getPeerId = async () => {
     let idListener = null
     if (!fs.existsSync("config.json")) {
-        idListener = await peerId.create({ bits: "2048", keyType: "secp256k1" })
+        idListener = await peerId.create({ bits: "2048", keyType: "RSA" })
         fs.writeFileSync("config.json", JSON.stringify(idListener.toJSON()))
     }
     idListener = JSON.parse(fs.readFileSync("config.json").toString())
     return peerId.createFromJSON(idListener)
 }
 
+const addPeer = async (node, addr) => {
+    try {
+        const connectNode = await node.dial(addr)
+        return connectNode
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+const removePeer = async (peer) => peer.close()
+
+const sendMsg = async (peer, msg) => {
+    try{
+        const {stream} = await peer.newStream("/chat")
+        await pipe(msg, stream)
+        stream.close()
+    }catch(err){
+        console.log(err.message)
+    }
+}
+
 (async () => {
     const peerId = await getPeerId()
     const node = await Libp2p.create({
         peerId,
-        addresses: { listen: ['/ip4/0.0.0.0/tcp/0'] },
+        addresses: { listen: ['/ip4/0.0.0.0/tcp/4002'] },
         modules: {
             transport: [TCP, WS],
             streamMuxer: [mplex],
@@ -41,27 +62,31 @@ const getPeerId = async () => {
         }
     })
     node.handle("/chat", async ({ connection, stream, protocol }) => {
-        console.log("connection", connection.remotePeer.toB58String())
-        pipe(stream, async (src) => {
-            for await (let msg of src) {
-                console.log(msg.toString())
+        console.log(`From peer: ${connection.remotePeer.toB58String()}`)
+        await pipe(stream, async (src) => {
+            try {
+                for await (let msg of src) {
+                    console.log(msg.toString())
+                }
+            } catch (err) {
+                console.log(err.message)
             }
         })
     })
 
-    node.connectionManager.on("peer:connect", (connection) => console.log('connected to: ', connection.remotePeer.toB58String()))
-    node.on("peer:discovery", (peer) => console.log('Discovered:', peer))
-    await node.start()
-    console.log('listening on:', node.isStarted())
-    node.multiaddrs.forEach((ma) => console.log(`${ma.toString()}/p2p/${node.peerId.toB58String()}`))
-    let quest = createInterface({ input: process.stdin, output: process.stdout })
-    quest.question("Enter addr ", async (addr) => {
-        try {
-            const anotherNode = await node.dialProtocol(addr, "/chat")
-            await pipe(["Own protocol"], anotherNode.stream)
-            quest.close()
-        } catch (err) {
-            console.log(err.message)
-        }
+    node.connectionManager.on("peer:connect", (connection) => {
+        peerStreams.push(connection)
+        console.log('connected to: ', connection.remotePeer.toB58String())
     })
+    node.connectionManager.on("peer:disconnect", (connection) => console.log("disconnect", connection.remotePeer.toB58String()))
+    // node.on("peer:discovery", (peer) => console.log('Discovered:', peer))
+    await node.start()
+    console.log('is listening:', node.isStarted())
+    node.multiaddrs.forEach((ma) => console.log(`${ma.toString()}/p2p/${node.peerId.toB58String()}`))
+    const r = repl.start()
+    r.context["node"] = node
+    r.context["peers"] = peerStreams
+    r.context["addNode"] = (addr) => addPeer(node, addr)
+    r.context["removeNode"] = removePeer
+    r.context["send"] = sendMsg
 })()
